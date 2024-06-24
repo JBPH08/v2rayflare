1d00ff0f720ec9423ee8ae3b950423a71de78065
JBPH08 <88840147+JBPH08@users.noreply.github.com>
2024-06-24 20:25:09 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '8e4f105d-104c-42c9-b98d-5201276e2c3d';
+let userID = '8e4f105d-104c-42c9-b26d-5201276e2c3d';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
